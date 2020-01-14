3268a3be11b733d8e04c1c344ad3f6543a3aa6c7
Jurgen Azofeifa <jurgen.azofeifa@bluegreenvacations.com>
2020-01-14 11:08:25 -0600

    login elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Must import manually since sometimes it doesn't allow it or does it
+// Must add the httpClientModule in the imports array in order to use http client service from the import,
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      // Must add the http in order to use http client service (import above)
       HttpClientModule
    ],
    providers: [],
